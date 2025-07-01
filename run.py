17e72ba784bc31f3f039b2bb1b33cb2f418f9d8b
voice-error <prabin700003@gmail.com>
2025-07-01 16:24:08 +0545

    Add files via upload

---
+++ run.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from app.routes import login,userreg  # this will register your routes
+import os
+
+app = Flask(__name__, template_folder="app/templates", static_folder="app/static")
+import secrets
+app.config['SECRET_KEY'] = secrets.token_hex(16)
+
+print("TEMPLATE DIR:", os.path.abspath("app/templates"))
+print("Files in template dir:", os.listdir("app/templates"))
+
+# Register routes defined in login.py
+app.add_url_rule("/", view_func=login.index, methods=["GET", "POST"])
+app.add_url_rule("/login", view_func=login.index, methods=["GET", "POST"])
+app.add_url_rule("/userreg", view_func=userreg.register, methods=["GET", "POST"])
+
+if (__name__) == "__main__":
+    app.run(debug=True)
